c632aa36daea4fbe0655e6cb0f32b88531030ef6
Cuong <cuongnv.goat@gmail.com>
2024-07-10 08:22:28 +0700

    updata notif.css

--- src/pages/Notifications.jsx
+++ src/pages/Notifications.jsx
@@ -5,6 +5,7 @@ import "./CSS/Notification.css";
 const Notifications = () => {
   const [notifications, setNotifications] = useState([]);
   const [message, setMessage] = useState("");
+  const [readNotif, setReadNotif] = useState("");
   const token = localStorage.getItem("token");
 
   const getNotification = async () => {
@@ -30,19 +31,33 @@ const Notifications = () => {
           Authorization: `Bearer ${token}`,
         },
       });
+      // const data = await response.json();
+      // setNotifications(data);
 
       // Cập nhật trạng thái của thông báo trong mảng notifications
-      setNotifications((prevNotifications) =>
-        prevNotifications.map((notif) => (notif._id === id ? { ...notif, isRead: true } : notif))
-      );
+      // setNotifications((prevNotifications) =>
+      //   prevNotifications.map((notif) => (notif._id === id ? { ...notif, isRead: true } : notif))
+      // );
+      // getNotification();
     } catch (error) {
       console.error("Failed to mark notification as read", error);
     }
   };
+  const handleDeleteNotification = async () => {
+    try {
+      const response = await fetch("http://localhost:3001/notifications/delete", {
+        method: "Delete",
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      });
+      getNotification();
+    } catch (error) {}
+  };
 
   useEffect(() => {
     getNotification();
-  }, []);
+  }, [readNotif]);
 
   return (
     <div className="notification">
@@ -52,7 +67,10 @@ const Notifications = () => {
             key={notif._id}
             onClick={() => {
               setMessage(notif.message);
+
               handleReadNotification(notif._id);
+              setReadNotif(notif._id);
+              // getNotification();
             }}
             className={notif.isRead ? "" : "bold"}
           >
@@ -60,6 +78,9 @@ const Notifications = () => {
           </li>
         ))}
       </ul>
+      <button className="delete-notif" onClick={() => handleDeleteNotification()}>
+        Xóa Thông báo đã đọc
+      </button>
       <div className="notification-content">{message && <div className="show-content">{message}</div>}</div>
     </div>
   );
--- src/pages/Thesis.jsx
+++ src/pages/Thesis.jsx
@@ -7,8 +7,21 @@ import ThesisTable from "../components/ThesisTable/ThesisTable.jsx";
 const Thesis = () => {
   const [showForm, setShowForm] = useState(false);
   const [theses, setTheses] = useState([]);
+  const [deadline, setDeadline] = useState("");
   const role = localStorage.getItem("role");
   const token = localStorage.getItem("token");
+
+  const formatDate = (dateString) => {
+    const date = new Date(dateString);
+    return date.toLocaleString("en-GB", {
+      timeZone: "Asia/Ho_Chi_Minh",
+      year: "numeric",
+      month: "2-digit",
+      day: "2-digit",
+      hour: "2-digit",
+      minute: "2-digit",
+    });
+  };
 
   const fetchTheses = async () => {
     try {
@@ -24,9 +37,24 @@ const Thesis = () => {
       console.error("Failed to fetch theses", error);
     }
   };
+  const fetchDeadline = async () => {
+    try {
+      const response = await fetch("http://localhost:3001/deadlines", {
+        method: "GET",
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      });
+      const data = await response.json();
+      setDeadline(formatDate(data.endDate));
+    } catch (error) {
+      console.error("Failed to fetch Deadline", error);
+    }
+  };
 
   useEffect(() => {
     fetchTheses();
+    fetchDeadline();
   }, []);
 
   return (
@@ -36,6 +64,7 @@ const Thesis = () => {
           Thêm mới
         </button>
       )}
+      <h2>Deadline: {deadline !== "Invalid Date" && deadline}</h2>
       {showForm && <AddThesisForm onClose={() => setShowForm(false)} fetchTheses={fetchTheses} />}
       <ThesisTable theses={theses} fetchTheses={fetchTheses} />
     </div>
