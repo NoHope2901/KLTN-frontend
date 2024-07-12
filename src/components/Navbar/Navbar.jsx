b73a1b272632c81b2ebead330d4198c0fe8685d8
Cuong <cuongnv.goat@gmail.com>
2024-07-12 11:34:07 +0700

    update navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -27,50 +27,35 @@ const Navbar = () => {
             </div>
             <ul className="nav-menu">
               <li>
-<<<<<<< HEAD
-                <NavLink activeClassName="active" style={{ textDecoration: "none" }} to="/">
-                  <i class="bx bx-home"></i>
-                  Trang chủ
-=======
                 <NavLink
                   className={({ isActive }) => (isActive ? "active" : "none")}
                   style={{ textDecoration: "none" }}
-                  to="/">
+                  to="/"
+                >
                   <i className="bx bx-home"></i>
-                  Home
->>>>>>> 237e6c0ea1833ffa49e36b002f33d1950ebe0d56
+                  Trang chủ
                 </NavLink>
               </li>
               {role !== "admin" && (
                 <li>
-<<<<<<< HEAD
-                  <NavLink activeClassName="active" style={{ textDecoration: "none" }} to="/notifications">
-                    <i class="bx bx-bell-minus"></i>
-                    Thông báo
-=======
                   <NavLink
                     className={({ isActive }) => (isActive ? "active" : "none")}
                     style={{ textDecoration: "none" }}
-                    to="/notifications">
+                    to="/notifications"
+                  >
                     <i className="bx bx-bell-minus"></i>
-                    Notifications
->>>>>>> 237e6c0ea1833ffa49e36b002f33d1950ebe0d56
+                    Thông Báo
                   </NavLink>
                 </li>
               )}
               <li>
-<<<<<<< HEAD
-                <NavLink activeClassName="active" style={{ textDecoration: "none" }} to="/theses">
-                  <i class="bx bx-book-open"></i>
-                  Khóa luận
-=======
                 <NavLink
                   className={({ isActive }) => (isActive ? "active" : "none")}
                   style={{ textDecoration: "none" }}
-                  to="/theses">
-                  <i className="bx bx-group"></i>
-                  Theses
->>>>>>> 237e6c0ea1833ffa49e36b002f33d1950ebe0d56
+                  to="/theses"
+                >
+                  <i className="bx bx-book-open"></i>
+                  Khóa Luận
                 </NavLink>
               </li>
               {role === "admin" && (
@@ -78,7 +63,8 @@ const Navbar = () => {
                   <NavLink
                     className={({ isActive }) => (isActive ? "active" : "none")}
                     style={{ textDecoration: "none" }}
-                    to="/admin">
+                    to="/admin"
+                  >
                     <i className="bx bx-child"></i>
                     Admin
                   </NavLink>
@@ -86,34 +72,37 @@ const Navbar = () => {
               )}
               {role === "teacher" && (
                 <li>
-<<<<<<< HEAD
-                  <NavLink activeClassName="active" style={{ textDecoration: "none" }} to="/teacher">
-                    <i class="bx bxs-graduation"></i>
-                    Giáo viên
+                  <NavLink
+                    className={({ isActive }) => (isActive ? "active" : "none")}
+                    style={{ textDecoration: "none" }}
+                    to="/teacher"
+                  >
+                    <i className="bx bxs-graduation"></i>
+                    Giáo Viên
                   </NavLink>
                 </li>
               )}
               {role === "teacher" && (
                 <li>
-                  <NavLink activeClassName="active" style={{ textDecoration: "none" }} to="/council">
-                    <i class="bx bxs-group"></i>
+                  <NavLink
+                    className={({ isActive }) => (isActive ? "active" : "none")}
+                    style={{ textDecoration: "none" }}
+                    to="/council"
+                  >
+                    <i className="bx bx-group"></i>
                     Hội Đồng BV
                   </NavLink>
                 </li>
               )}
               {role === "student" && (
                 <li>
-                  <NavLink activeClassName="active" style={{ textDecoration: "none" }} to="/submit">
-                    <i class="bx bx-upload"></i>
-                    Nộp bài tập
-=======
                   <NavLink
                     className={({ isActive }) => (isActive ? "active" : "none")}
                     style={{ textDecoration: "none" }}
-                    to="/teacher">
-                    <i className="bx bxs-graduation"></i>
-                    Teacher
->>>>>>> 237e6c0ea1833ffa49e36b002f33d1950ebe0d56
+                    to="/submit"
+                  >
+                    <i className="bx bx-upload"></i>
+                    Nộp Tài Liệu
                   </NavLink>
                 </li>
               )}
@@ -123,8 +112,7 @@ const Navbar = () => {
             <img src={require("../images/no-avt.png")} alt="" />
             {role !== "admin" && (
               <p>
-                {localStorage.getItem("fullname")} -{" "}
-                {localStorage.getItem("code")}
+                {localStorage.getItem("fullname")} - {localStorage.getItem("code")}
               </p>
             )}
 
